48ac54505e3dc5c009c9a4a40a2a8a80627653e7
LaSan99 <lasannavodya@gmail.com>
2026-02-10 11:51:41 +0530

    b1

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
-  } catch (err) {
-    console.error("MongoDB error:", err.message);
-    process.exit(1);
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI is missing in environment variables");
   }
+
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("MongoDB connected");
 };
 
 module.exports = connectDB;
